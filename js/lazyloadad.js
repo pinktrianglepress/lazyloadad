474d55302049f39ad25a9f857da593accc364b38
Carol Levesque <carol.levesque@xtra.ca>
2013-09-24 12:14:16 -0400

    Fixing loading of ads issue with new Ilya settings. Changed threshold to 2000 & timeout to 800

--- js/lazyloadad.js
+++ js/lazyloadad.js
@@ -2,11 +2,11 @@
 (function($) { 
   $(document).ready(function() {
     $(Drupal.settings.lazyloadad).lazyLoadAd({
-			threshold     : 500,         // You can set threshold on how close to the edge ad should come before it is loaded. Default is 0 (when it is visible).
+			threshold     : 2000,         // You can set threshold on how close to the edge ad should come before it is loaded. Default is 0 (when it is visible).
 			forceLoad    : false,       // Ad is loaded even if not visible. Default is false.
 			onLoad        : false,      // Callback function on call ad loading
 			onComplete : false,      // Callback function when load is loaded
-			timeout        : 5,          // Timeout ad load
+			timeout        : 800,          // Timeout ad load
 			debug          : false,    // For debug use : draw colors border depends on load status
 			xray             : false
 		});  
