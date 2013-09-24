fba6265dec2a273aa4d185fe2d3bd02980aea468
Carol Levesque <carol.levesque@xtra.ca>
2013-09-24 14:53:52 -0400

    iss 762 - These are Ilya's 2nd recommendation - Carol no likey

--- js/lazyloadad.js
+++ js/lazyloadad.js
@@ -2,11 +2,11 @@
 (function($) { 
   $(document).ready(function() {
     $(Drupal.settings.lazyloadad).lazyLoadAd({
-			threshold     : 2000,         // You can set threshold on how close to the edge ad should come before it is loaded. Default is 0 (when it is visible).
-			forceLoad    : false,       // Ad is loaded even if not visible. Default is false.
+			threshold     : 0,         // You can set threshold on how close to the edge ad should come before it is loaded. Default is 0 (when it is visible).
+			forceLoad    : true,       // Ad is loaded even if not visible. Default is false.
 			onLoad        : false,      // Callback function on call ad loading
 			onComplete : false,      // Callback function when load is loaded
-			timeout        : 800,          // Timeout ad load
+			timeout        : 5,          // Timeout ad load
 			debug          : false,    // For debug use : draw colors border depends on load status
 			xray             : false
 		});  
