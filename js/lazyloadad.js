490861826412292650edc9069d77694897069479
Carol Levesque <carol.levesque@xtra.ca>
2013-09-27 09:48:36 -0400

    lazyloading issue - added more settings & removed some settings. May not be final, but works in my environment

--- js/lazyloadad.js
+++ js/lazyloadad.js
@@ -2,13 +2,10 @@
 (function($) { 
   $(document).ready(function() {
     $(Drupal.settings.lazyloadad).lazyLoadAd({
-			threshold     : 0,         // You can set threshold on how close to the edge ad should come before it is loaded. Default is 0 (when it is visible).
 			forceLoad    : true,       // Ad is loaded even if not visible. Default is false.
-			onLoad        : false,      // Callback function on call ad loading
-			onComplete : false,      // Callback function when load is loaded
-			timeout        : 5,          // Timeout ad load
+			timeout        : 0,          // Timeout ad load
+			syncAds: true,
 			debug          : false,    // For debug use : draw colors border depends on load status
-			xray             : false
 		});  
   });
 })(jQuery); 
